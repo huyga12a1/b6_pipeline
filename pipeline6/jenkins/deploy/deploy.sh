d135f956bd4ac3e6c9e98c81c72b1427a898f9f1
huyga12a1 <81863478+huyga12a1@users.noreply.github.com>
2021-12-19 15:46:03 +0700

    Update deploy.sh

--- pipeline6/jenkins/deploy/deploy.sh
+++ pipeline6/jenkins/deploy/deploy.sh
@@ -3,6 +3,7 @@ export PASS=dantruong
 echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
-scp /tmp/.auth root@192.168.139.130:/tmp/.auth
-scp start_app.sh root@192.168.139.130:/tmp/start_app.sh
-ssh root@192.168.139.130 "bash /tmp/start_app.sh"
+# scp /tmp/.auth root@192.168.139.130:/tmp/.auth
+# scp start_app.sh root@192.168.139.130:/tmp/start_app.sh
+# ssh root@192.168.139.130 "bash /tmp/start_app.sh"
+bash ./pipeline6/jenkins/deploy/start_app.sh
